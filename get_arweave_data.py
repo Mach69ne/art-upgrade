cccba4ad47c456bc06acd2724e4d9914d2a811a4
Mach69ne <yexpressiscoming@gmail.com>
2022-10-27 23:07:34 +0200

    test

--- get_arweave_data.py
+++ get_arweave_data.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import json
-def scraper(url):
+def scraper(url,mint):
     url = requests.get(url)
     url.status_code
     url.headers
@@ -9,5 +9,14 @@ def scraper(url):
     soup = BeautifulSoup(e, "html.parser")
     soup = str(soup)
     soup = json.loads(soup)
+    number = soup['name'].replace("Forest Apes #","")
+    soup["image"] = "https://raw.githubusercontent.com/Sweatyfish/Forestapes-Gen1/main/"+number+".png"
+    properties = soup['properties']
+    with open(mint+".json","w") as f:
+        json.dump(soup,f)
+    f.close()
+
+
 url = "https://arweave.net/-fMZVF026AUBuDilzc7pNt5kBq7-qN5eYdw5hY3ms20"
-scraper(url)
+mint = "3jrGzyQHfRqepKQosFPBJruTnLKNdMf2RzfotQAxMd5o"
+scraper(url,mint)
