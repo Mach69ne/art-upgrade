2cce4cc40be2a5426a501d003d4c891339eff2e1
Mach69ne <yexpressiscoming@gmail.com>
2022-10-27 22:53:58 +0200

    u

---
+++ get_arweave_data.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+def scraper(url):
+    url = requests.get(url)
+    url.status_code
+    url.headers
+    e = url.content
+    soup = BeautifulSoup(e, "html.parser")
+    soup = dict(soup)
+    print(soup)
+url = "https://arweave.net/-fMZVF026AUBuDilzc7pNt5kBq7-qN5eYdw5hY3ms20"
+scraper(url)
