b7f491f0f7dddd74457705a4e68985e0e78bd511
Mach69ne <yexpressiscoming@gmail.com>
2022-11-13 22:15:11 +0100

    fix cig, +nft not works

--- update_traits.py
+++ update_traits.py
@@ -29,8 +29,8 @@ def getdata():
                         if current_att['value']:
                             value = current_att['value']
                             print(current_att['value'])
-                            if value=="GREY":
-                                current_att['value']="Grey"
+                            if value=="Cig":
+                                current_att['value']="Cigarette"
                             print(current_att['value'])
                             attributes[k] = current_att
                     data['attributes'] = attributes
